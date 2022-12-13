aecda1e7bc9049e15d3402445d023bbe82ce66e0
Sandy Zhang <sandyzhang142@gmail.com>
2022-12-13 18:00:39 -0500

    commit tree 3d0c8e5aeadfc069beab5f859757699b844662ec

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
-
+    # setter method
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+    def job
+        @job
+      end
+    
+      def job=(job)
+        @job = job
+      end
 end
+
+brennan = Person.new
+brennan.name=("Brennan")
+
+brennan.name
